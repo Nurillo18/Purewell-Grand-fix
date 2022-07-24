48434b2aa93c60598ef2eac6dc6d7fed1da0bc61
Nurillo18 <suyunboyevnurillo18@gmail.com>
2022-07-24 18:46:49 +0500

    Add last change

--- js/app.js
+++ js/app.js
@@ -4,13 +4,40 @@ var elHamLine1 = document.querySelector(".line1")
 var elHamLine2 = document.querySelector(".line2")
 var elModal = document.querySelector(".modal")
 var elHeader = document.querySelector(".site-header")
+
+var elModalLink = document.querySelector(".modal__link")
+var elModalLink1 = document.querySelector(".modal1")
+var elModalLink2 = document.querySelector(".modal2")
+var elModalLink3 = document.querySelector(".modal3")
+
+
 elHamburger.addEventListener("click", function() {
     elModal.classList.toggle("open")
     elHamLine.classList.toggle("rotate-open1")
     elHamLine1.classList.toggle("rotate")
     elHamLine2.classList.toggle("rotate-open2")
 } )
-
+elModalLink.addEventListener("click", function() {
+    elModal.classList.remove("open")
+    elHamLine.classList.remove("rotate-open1")
+    elHamLine1.classList.remove("rotate")
+    elHamLine2.classList.remove("rotate-open2")
+})
+elModalLink1.addEventListener("click", function() {
+    elModal.classList.remove("open")
+    elHamLine.classList.remove("rotate-open1")
+    elHamLine1.classList.remove("rotate")
+    elHamLine2.classList.remove("rotate-open2")
+})
+elModalLink2.addEventListener("click", function() {
+    elModal.classList.remove("open")
+    elHamLine.classList.remove("rotate-open1")
+    elHamLine1.classList.remove("rotate")
+    elHamLine2.classList.remove("rotate-open2")
+})
+elModalLink3.addEventListener("click", function() {
+    elModal.classList.remove("open")
+})
 window.addEventListener("scroll", function() {
     elHeader.classList.toggle("sticky", window.scrollY > 0)
 
